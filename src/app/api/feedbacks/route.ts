377cdfc04e3cae2ecfb6affd1925d7b3d7307552
Willian Bretzke <willianbretzke2013@hotmail.com>
2024-09-09 23:34:47 -0300

    Added user feedback api route

---
+++ src/app/api/feedbacks/route.ts
@@ -0,0 +1,29 @@
+import prisma from "@/utils/db";
+import { randomUUID } from "node:crypto";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+  const feedbacks = await prisma.userFeedback.findMany({
+    where: {
+      active: true,
+    },
+  });
+
+  return NextResponse.json({ feedbacks }, { status: 200 });
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  await prisma.userFeedback.create({
+    data: {
+      id: randomUUID(),
+      name: body.name,
+      email: body.email,
+      message: body.message,
+      imageUrl: body.imageUrl,
+    },
+  });
+
+  return NextResponse.json({}, { status: 201 });
+}
