179bcf6a9ae5d1d53068eeec5e87a745a1dda6e2
Willian Bretzke <willianbretzke2013@hotmail.com>
2024-09-21 16:09:41 -0300

    Changed project description

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { useLocale } from "next-intl";
 import { useRouter } from "next/navigation";
 
 export default function NotFound() {
   const router = useRouter();
+  const locale = useLocale();
 
-  router.push("/");
+  router.push(`/${locale}`);
 }
